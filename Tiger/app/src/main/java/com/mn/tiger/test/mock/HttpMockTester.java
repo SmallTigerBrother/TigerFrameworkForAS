bdf303b4750802535f5a13d785df0d58d24da70f
smallTigerBrother <664674103@qq.com>
2015-09-02 16:53:57 +0800

    修改mock测试数据来源为asset

--- Tiger/app/src/main/java/com/mn/tiger/test/mock/HttpMockTester.java
+++ Tiger/app/src/main/java/com/mn/tiger/test/mock/HttpMockTester.java
@@ -9,6 +9,7 @@ import com.mn.tiger.log.Logger;
 import com.mn.tiger.request.error.TGHttpError;
 import com.mn.tiger.request.receiver.TGHttpResult;
 import com.mn.tiger.request.sync.ApacheSyncHttpLoader;
+import com.mn.tiger.request.sync.OkHttpSyncHttpLoader;
 import com.mn.tiger.utility.FileUtils;
 
 public class HttpMockTester
@@ -26,39 +27,36 @@ public class HttpMockTester
 	
 	public static TGHttpResult getMockTestData(String url)
 	{
-		TGHttpResult httpResult = getDefaultHttpResult();
+		TGHttpResult httpResult = null;
+
+		String dataKey = dataMap.get(url);
+
+		if(dataKey.startsWith("http://") || dataKey.startsWith("https://"))
+		{
+			httpResult = new OkHttpSyncHttpLoader().loadByGetSync(
+					TGApplication.getInstance(), dataKey, null, null);
+		}
+		else
+		{
+			httpResult = new TGHttpResult();
+			String data = FileUtils.readStringFromAsset(TGApplication.getInstance(), dataKey + ".json");
+			httpResult.setResponseCode(200);
+			httpResult.setResult(data);
+		}
+
+		LOG.d("[Method:getMockTestData] url : " + url + "\n" + "result : " + httpResult.getResult());
+		return httpResult;
+	}
+
+	public static boolean isTestAble(String url)
+	{
 		if(null != dataMap && TEST_ABLE)
 		{
 			String dataKey = dataMap.get(url);
-			if(!TextUtils.isEmpty(dataKey))
-			{
-				if(dataKey.startsWith("http://") || dataKey.startsWith("https://"))
-				{
-					httpResult = new ApacheSyncHttpLoader().loadByGetSync(
-							TGApplication.getInstance(), dataKey, null, null);
-				}
-				else
-				{
-					String data = FileUtils.readStringFromAsset(TGApplication.getInstance(), dataKey + ".json");
-					httpResult.setResponseCode(200);
-					httpResult.setResult(data);
-				}
-				
-				LOG.d("[Method:getMockTestData] url : " + url + "\n" + "result : " + httpResult.getResult());
-				return httpResult;
-			}
+			return !TextUtils.isEmpty(dataKey);
 		}
-		
-		return httpResult;
+
+		return false;
 	}
-	
-	private static TGHttpResult getDefaultHttpResult()
-	{
-		TGHttpResult httpResult = new TGHttpResult();
-		httpResult.setResponseCode(TGHttpError.MOCK_TEST_ERROR);
-		httpResult.setResult(TGHttpError.getDefaultErrorMsg(TGApplication.getInstance(), TGHttpError.MOCK_TEST_ERROR));
-		return httpResult;
-	}
-	
-	
+
 }
