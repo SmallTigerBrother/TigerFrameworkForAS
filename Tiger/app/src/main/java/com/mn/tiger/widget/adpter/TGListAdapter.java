511b18b4014441c46f30adcf3c6d2f64d9c938ec
smallTigerBrother <664674103@qq.com>
2015-09-02 16:53:43 +0800

    新增updatePartData方法

--- Tiger/app/src/main/java/com/mn/tiger/widget/adpter/TGListAdapter.java
+++ Tiger/app/src/main/java/com/mn/tiger/widget/adpter/TGListAdapter.java
@@ -22,445 +22,480 @@ import java.util.List;
  */
 public class TGListAdapter<T> extends BaseAdapter
 {
-	/**
-	 * 日志标签
-	 */
-	protected final String LOG_TAG = this.getClass().getSimpleName();
+    /**
+     * 日志标签
+     */
+    protected final String LOG_TAG = this.getClass().getSimpleName();
 
-	/**
-	 * 运行环境
-	 */
-	private Context context;
+    /**
+     * 运行环境
+     */
+    private Context context;
 
-	/**
-	 * 列表填充数据
-	 */
-	private List<T> items = null;
+    /**
+     * 列表填充数据
+     */
+    private List<T> items = null;
 
-	/**
-	 * 列表行视图layoutId
-	 */
-	private int convertViewLayoutId;
+    /**
+     * 列表行视图layoutId
+     */
+    private int convertViewLayoutId;
 
-	/**
-	 * viewholder类，用于视图重用，初始化列表行和填充列表行数据
-	 */
-	private Class<? extends TGViewHolder<T>> viewHolderClass;
+    /**
+     * viewholder类，用于视图重用，初始化列表行和填充列表行数据
+     */
+    private Class<? extends TGViewHolder<T>> viewHolderClass;
 
-	private Handler handler;
+    private Handler handler;
 
-	/**
-	 * 可用于adapter、viewholder之间传值
-	 */
-	private Object tag;
+    /**
+     * 可用于adapter、viewholder之间传值
+     */
+    private Object tag;
 
-	private boolean strictlyReuse = false;
+    private boolean strictlyReuse = false;
 
-	private SparseArray<View> convertViews;
+    private SparseArray<View> convertViews;
 
-	/**
-	 * @param context
-	 * @param items               列表填充数据
-	 * @param convertViewLayoutId 列表行视图layoutId
-	 * @param viewHolderClass     ViewHolder类名
-	 */
-	public TGListAdapter(Context context, List<T> items, int convertViewLayoutId,
-						 Class<? extends TGViewHolder<T>> viewHolderClass)
-	{
-		this.context = context;
-		this.items = new ArrayList<T>();
-		if (null != items)
-		{
-			this.items.addAll(items);
-		}
+    /**
+     * @param context
+     * @param items               列表填充数据
+     * @param convertViewLayoutId 列表行视图layoutId
+     * @param viewHolderClass     ViewHolder类名
+     */
+    public TGListAdapter(Context context, List<T> items, int convertViewLayoutId,
+                         Class<? extends TGViewHolder<T>> viewHolderClass)
+    {
+        this.context = context;
+        this.items = new ArrayList<T>();
+        if (null != items)
+        {
+            this.items.addAll(items);
+        }
 
-		this.convertViewLayoutId = convertViewLayoutId;
-		this.viewHolderClass = viewHolderClass;
+        this.convertViewLayoutId = convertViewLayoutId;
+        this.viewHolderClass = viewHolderClass;
 
-		//根据内存容量判断是否启用严格重用
-		setStrictlyReuse(false);
-	}
+        //根据内存容量判断是否启用严格重用
+        setStrictlyReuse(false);
+    }
 
-	public void setStrictlyReuse(boolean strictlyReuse)
-	{
-		this.strictlyReuse = strictlyReuse;
-		if(!strictlyReuse)
-		{
-			convertViews = new SparseArray<View>();
-		}
-		else
-		{
-			convertViews.clear();
-		}
-	}
+    public void setStrictlyReuse(boolean strictlyReuse)
+    {
+        this.strictlyReuse = strictlyReuse;
+        if(!strictlyReuse)
+        {
+            convertViews = new SparseArray<View>();
+        }
+        else
+        {
+            convertViews.clear();
+        }
+    }
 
-	/**
-	 * @see BaseAdapter#getCount()
-	 */
-	@Override
-	public int getCount()
-	{
-		return items.size();
-	}
+    /**
+     * @see BaseAdapter#getCount()
+     */
+    @Override
+    public int getCount()
+    {
+        return items.size();
+    }
 
-	/**
-	 * @see BaseAdapter#getItem(int)
-	 */
-	@Override
-	public Object getItem(int position)
-	{
-		return items.get(position);
-	}
+    /**
+     * @see BaseAdapter#getItem(int)
+     */
+    @Override
+    public Object getItem(int position)
+    {
+        return items.get(position);
+    }
 
-	/**
-	 * @see BaseAdapter#getItemId(int)
-	 */
-	@Override
-	public long getItemId(int position)
-	{
-		return position;
-	}
+    /**
+     * @see BaseAdapter#getItemId(int)
+     */
+    @Override
+    public long getItemId(int position)
+    {
+        return position;
+    }
 
-	/**
-	 * @see BaseAdapter#getView(int, View, ViewGroup)
-	 */
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent)
-	{
-		if(!strictlyReuse)
-		{
-			convertView = convertViews.get(position);
-		}
+    /**
+     * @see BaseAdapter#getView(int, View, ViewGroup)
+     */
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        if(!strictlyReuse)
+        {
+            convertView = convertViews.get(position);
+        }
 
-		if (null == convertView)
-		{
-			convertView = initView(parent, position);
+        if (null == convertView)
+        {
+            convertView = initView(parent, position);
 
-			if(!strictlyReuse)
-			{
-				convertViews.put(position, convertView);
-				fillData(position, convertView, parent);
-			}
-		}
-		else
-		{
-			if(strictlyReuse)
-			{
-				fillData(position, convertView, parent);
-			}
-		}
+            if(!strictlyReuse)
+            {
+                convertViews.put(position, convertView);
+                fillData(position, convertView, parent);
+            }
+        }
+        else
+        {
+            if(strictlyReuse)
+            {
+                fillData(position, convertView, parent);
+            }
+        }
 
-		return convertView;
-	}
+        return convertView;
+    }
 
-	/**
-	 * 初始化convertView（仅在无法重用视图时调用）
-	 *
-	 * @return
-	 */
-	protected View initView(ViewGroup parent, int position)
-	{
-		TGViewHolder<T> viewHolder = null;
-		View convertView = null;
-		if (convertViewLayoutId > 0)
-		{
-			try
-			{
-				convertView = LayoutInflater.from(context).inflate(convertViewLayoutId, null);
-			}
-			catch (Exception e)
-			{
-				throw new RuntimeException(e);
-			}
-		}
+    /**
+     * 初始化convertView（仅在无法重用视图时调用）
+     *
+     * @return
+     */
+    protected View initView(ViewGroup parent, int position)
+    {
+        TGViewHolder<T> viewHolder = null;
+        View convertView = null;
+        if (convertViewLayoutId > 0)
+        {
+            try
+            {
+                convertView = LayoutInflater.from(context).inflate(convertViewLayoutId, null);
+            }
+            catch (Exception e)
+            {
+                throw new RuntimeException(e);
+            }
+        }
 
-		viewHolder = initViewHolder();
-		viewHolder.setLayoutId(convertViewLayoutId);
-		convertView = viewHolder.initView(convertView, parent, position);
-		convertView.setTag(viewHolder);
+        viewHolder = initViewHolder();
+        viewHolder.setLayoutId(convertViewLayoutId);
+        convertView = viewHolder.initView(convertView, parent, position);
+        convertView.setTag(viewHolder);
 
-		return convertView;
-	}
+        return convertView;
+    }
 
-	/**
-	 * 填充列表行数据（每次调用getView时都会调用）
-	 *
-	 * @param position
-	 * @param convertView
-	 * @param parent
-	 */
-	@SuppressWarnings("unchecked")
-	protected void fillData(int position, View convertView, ViewGroup parent)
-	{
-		TGViewHolder<T> viewHolder = (TGViewHolder<T>) convertView.getTag();
-		//填充列表行数据
-		viewHolder.fillData(parent, convertView, items.get(position), position);
+    /**
+     * 填充列表行数据（每次调用getView时都会调用）
+     *
+     * @param position
+     * @param convertView
+     * @param parent
+     */
+    @SuppressWarnings("unchecked")
+    protected void fillData(int position, View convertView, ViewGroup parent)
+    {
+        TGViewHolder<T> viewHolder = (TGViewHolder<T>) convertView.getTag();
+        //填充列表行数据
+        viewHolder.fillData(parent, convertView, items.get(position), position);
 
-		//更新列表行尺寸
-		viewHolder.updateViewDimension(parent, convertView, items.get(position), position);
-	}
+        //更新列表行尺寸
+        viewHolder.updateViewDimension(parent, convertView, items.get(position), position);
+    }
 
-	/**
-	 * 初始化ViewHolder
-	 *
-	 * @return
-	 */
-	protected TGViewHolder<T> initViewHolder()
-	{
-		TGViewHolder<T> viewHolder = null;
-		try
-		{
-			viewHolder = viewHolderClass.newInstance();
-			viewHolder.setContext(context);
-			viewHolder.setAdapter(this);
-		}
-		catch (Exception e)
-		{
-			throw new RuntimeException(e);
-		}
+    /**
+     * 初始化ViewHolder
+     *
+     * @return
+     */
+    protected TGViewHolder<T> initViewHolder()
+    {
+        TGViewHolder<T> viewHolder = null;
+        try
+        {
+            viewHolder = viewHolderClass.newInstance();
+            viewHolder.setContext(context);
+            viewHolder.setAdapter(this);
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException(e);
+        }
 
-		return viewHolder;
-	}
+        return viewHolder;
+    }
 
-	/**
-	 * 更新一条数据
-	 * @param position
-	 * @param data
-	 */
-	public void updateData(int position, T data)
-	{
-		if(position >=0 && position < getCount() && null == data)
-		{
-			if(!strictlyReuse)
-			{
-				convertViews.remove(position);
-			}
-			this.items.set(position, data);
-			this.notifyDataSetChanged();
-		}
-	}
+    /**
+     * 更新一条数据
+     * @param position
+     * @param data
+     */
+    public void updateData(int position, T data)
+    {
+        if(position >=0 && position < getCount() && null == data)
+        {
+            if(!strictlyReuse)
+            {
+                convertViews.remove(position);
+            }
+            this.items.set(position, data);
+            this.notifyDataSetChanged();
+        }
+    }
 
-	/**
-	 * 该方法的作用:更新列表数据
-	 *
-	 * @param data 列表数据
-	 * @date 2013-1-17
-	 */
-	public void updateData(List<T> data)
-	{
-		if (null != data)
-		{
-			if(this.items != data)
-			{
-				this.items.clear();
-				this.items.addAll(data);
-			}
-			convertViews.clear();
-			notifyDataSetChanged();
-		}
-	}
+    /**
+     * 该方法的作用:更新列表数据
+     *
+     * @param data 列表数据
+     * @date 2013-1-17
+     */
+    public void updateData(List<T> data)
+    {
+        if (null != data)
+        {
+            if(this.items != data)
+            {
+                this.items.clear();
+                this.items.addAll(data);
+            }
+            convertViews.clear();
+            notifyDataSetChanged();
+        }
+    }
 
-	/**
-	 * 更新列表行数据
-	 *
-	 * @param data
-	 */
-	public void updateData(T[] data)
-	{
-		if (null != data)
-		{
-			this.items.clear();
-			this.items.addAll(Arrays.asList(data));
-			convertViews.clear();
-			notifyDataSetChanged();
-		}
-	}
+    /**
+     * 更新列表行数据
+     *
+     * @param data
+     */
+    public void updateData(T[] data)
+    {
+        if (null != data)
+        {
+            this.items.clear();
+            this.items.addAll(Arrays.asList(data));
+            convertViews.clear();
+            notifyDataSetChanged();
+        }
+    }
 
-	/**
-	 * 局部刷新，data必需是列表中数据的一部分
-	 * @param data
-	 */
-	public void updatePartData(List<T> data)
-	{
-		if (null != data)
-		{
-			if(this.items != data)
-			{
-				for (int i = 0; i < data.size(); i++)
-				{
-					T dataItem = data.get(i);
-					int position = this.items.indexOf(dataItem);
-					if(position > -1)
-					{
-						this.items.set(position, dataItem);
-						convertViews.remove(position);
-					}
-					else
-					{
-						this.items.add(dataItem);
-					}
-				}
-			}
-			notifyDataSetChanged();
-		}
-	}
+    /**
+     * 局部刷新，data必需是列表中数据的一部分
+     * @param data
+     */
+    public void updatePartData(List<T> data)
+    {
+        if (null != data)
+        {
+            if(this.items != data)
+            {
+                for (int i = 0; i < data.size(); i++)
+                {
+                    T dataItem = data.get(i);
+                    int position = this.items.indexOf(dataItem);
+                    if(position > -1)
+                    {
+                        this.items.set(position, dataItem);
+                        convertViews.remove(position);
+                    }
+                    else
+                    {
+                        this.items.add(dataItem);
+                    }
+                }
+            }
+            notifyDataSetChanged();
+        }
+    }
 
-	/**
-	 * 局部刷新，data必需是列表中数据的一部分
-	 * @param data
-	 */
-	public void updatePartData(T[] data)
-	{
-		if (null != data)
-		{
-			for (int i = 0; i < data.length; i++)
-			{
-				T dataItem = data[i];
-				int position = this.items.indexOf(dataItem);
-				if(position > -1)
-				{
-					this.items.set(position, dataItem);
-					convertViews.remove(position);
-				}
-				else
-				{
-					this.items.add(dataItem);
-				}
-			}
-			notifyDataSetChanged();
-		}
-	}
+    /**
+     * 局部刷新，data必需是列表中数据的一部分
+     * @param data
+     */
+    public void updatePartData(T[] data)
+    {
+        if (null != data)
+        {
+            for (int i = 0; i < data.length; i++)
+            {
+                T dataItem = data[i];
+                int position = this.items.indexOf(dataItem);
+                if(position > -1)
+                {
+                    this.items.set(position, dataItem);
+                    convertViews.remove(position);
+                }
+                else
+                {
+                    this.items.add(dataItem);
+                }
+            }
+            notifyDataSetChanged();
+        }
+    }
 
-	/**
-	 * 向列表行追加数据
-	 *
-	 * @param data
-	 */
-	public void appendData(List<T> data)
-	{
-		if (null != data)
-		{
-			this.items.addAll(data);
-			notifyDataSetChanged();
-		}
-	}
+    /**
+     * 局部刷新，data必需是列表中数据的一个
+     * @param data
+     */
+    public void updatePartData(T data)
+    {
+        if (null != data)
+        {
+            int position = this.items.indexOf(data);
+            if(position > -1)
+            {
+                this.items.set(position, data);
+                convertViews.remove(position);
+            }
+            else
+            {
+                this.items.add(data);
+            }
+            notifyDataSetChanged();
+        }
+    }
 
-	/**
-	 * 向列表行追加数据
-	 *
-	 * @param data
-	 */
-	public void appendData(T[] data)
-	{
-		if (null != data)
-		{
-			this.items.addAll(Arrays.asList(data));
-			notifyDataSetChanged();
-		}
-	}
+    /**
+     * 向列表行追加数据
+     *
+     * @param data
+     */
+    public void appendData(List<T> data)
+    {
+        if (null != data)
+        {
+            this.items.addAll(data);
+            notifyDataSetChanged();
+        }
+    }
 
-	/**
-	 * 该方法的作用:
-	 * 获取列表数据
-	 *
-	 * @return
-	 * @date 2014年2月10日
-	 */
-	public List<T> getListItems()
-	{
-		return this.items;
-	}
+    /**
+     * 向列表行追加数据
+     *
+     * @param data
+     */
+    public void appendData(T[] data)
+    {
+        if (null != data)
+        {
+            this.items.addAll(Arrays.asList(data));
+            notifyDataSetChanged();
+        }
+    }
 
-	/**
-	 * 获取列表第一个元素
-	 *
-	 * @return 若列表无数据，返回null
-	 */
-	public T getFirstItem()
-	{
-		if (!items.isEmpty())
-		{
-			return items.get(0);
-		}
-		return null;
-	}
+    /**
+     * 向列表行追加数据
+     * @param data
+     */
+    public void appendData(T data)
+    {
+        if (null != data)
+        {
+            this.items.add(data);
+            notifyDataSetChanged();
+        }
+    }
 
-	/**
-	 * 获取列表最后一个元素
-	 *
-	 * @return 若列表无数据，返回null
-	 */
-	public T getLastItem()
-	{
-		if (!items.isEmpty())
-		{
-			return items.get(items.size() - 1);
-		}
-		return null;
-	}
+    /**
+     * 该方法的作用:
+     * 获取列表数据
+     *
+     * @return
+     * @date 2014年2月10日
+     */
+    public List<T> getListItems()
+    {
+        return this.items;
+    }
 
-	/**
-	 * 删除列表行
-	 *
-	 * @param position 列表行位置
-	 */
-	public void removeItem(int position)
-	{
-		if (items.size() > position && position >= 0)
-		{
-			items.remove(position);
-			convertViews.remove(position);
-			notifyDataSetChanged();
-		}
-	}
+    /**
+     * 获取列表第一个元素
+     *
+     * @return 若列表无数据，返回null
+     */
+    public T getFirstItem()
+    {
+        if (!items.isEmpty())
+        {
+            return items.get(0);
+        }
+        return null;
+    }
 
-	/**
-	 * 删除列表行
-	 *
-	 * @param item 列表行数据
-	 */
-	public void removeItem(T item)
-	{
-		if (items.contains(item))
-		{
-			items.remove(item);
-			convertViews.remove(items.indexOf(item));
-			notifyDataSetChanged();
-		}
-	}
+    /**
+     * 获取列表最后一个元素
+     *
+     * @return 若列表无数据，返回null
+     */
+    public T getLastItem()
+    {
+        if (!items.isEmpty())
+        {
+            return items.get(items.size() - 1);
+        }
+        return null;
+    }
 
-	/**
-	 * 清除所有数据
-	 */
-	public void removeAll()
-	{
-		items.clear();
-		convertViews.clear();
-		notifyDataSetChanged();
-	}
+    /**
+     * 删除列表行
+     *
+     * @param position 列表行位置
+     */
+    public void removeItem(int position)
+    {
+        if (items.size() > position && position >= 0)
+        {
+            items.remove(position);
+            convertViews.remove(position);
+            notifyDataSetChanged();
+        }
+    }
 
-	protected Context getContext()
-	{
-		return context;
-	}
+    /**
+     * 删除列表行
+     *
+     * @param item 列表行数据
+     */
+    public void removeItem(T item)
+    {
+        if (items.contains(item))
+        {
+            items.remove(item);
+            convertViews.remove(items.indexOf(item));
+            notifyDataSetChanged();
+        }
+    }
 
-	public Handler getHandler()
-	{
-		return handler;
-	}
+    /**
+     * 清除所有数据
+     */
+    public void removeAll()
+    {
+        items.clear();
+        convertViews.clear();
+        notifyDataSetChanged();
+    }
 
-	public void setHandler(Handler handler)
-	{
-		this.handler = handler;
-	}
+    protected Context getContext()
+    {
+        return context;
+    }
 
-	public Object getTag()
-	{
-		return tag;
-	}
+    public Handler getHandler()
+    {
+        return handler;
+    }
 
-	public void setTag(Object tag)
-	{
-		this.tag = tag;
-	}
+    public void setHandler(Handler handler)
+    {
+        this.handler = handler;
+    }
+
+    public Object getTag()
+    {
+        return tag;
+    }
+
+    public void setTag(Object tag)
+    {
+        this.tag = tag;
+    }
 }
