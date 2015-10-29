b4ad41221ef346c94766f47d18069633f4218d1d
SmallTigerBrother <664674103@qq.com>
2015-10-29 22:28:24 +0800

    新增TGMultiViewTypeRecyclerViewHolder类

---
+++ Tiger/app/src/main/java/com/mn/tiger/widget/recyclerview/TGMultiViewTypeRecyclerViewHolder.java
@@ -0,0 +1,41 @@
+package com.mn.tiger.widget.recyclerview;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Dalang on 2015/10/29.
+ */
+public abstract class TGMultiViewTypeRecyclerViewHolder extends TGRecyclerViewHolder<Object>
+{
+    private TGRecyclerViewHolder<Object> internalViewHolder;
+
+    @Override
+    public View initView(ViewGroup parent, int viewType)
+    {
+        internalViewHolder = initViewHolderByViewType(parent, viewType);
+
+        internalViewHolder.setContext(getContext());
+        internalViewHolder.setAdapter(getAdapter());
+        internalViewHolder.setLayoutId(getLayoutId());
+        internalViewHolder.setOnItemClickListener(getOnItemClickListener());
+        internalViewHolder.setRecyclerView((RecyclerView) parent);
+
+        return internalViewHolder.initView(parent, viewType);
+    }
+
+    protected abstract TGRecyclerViewHolder<Object> initViewHolderByViewType(ViewGroup parent, int viewType);
+
+    @Override
+    protected void updateViewDimension(ViewGroup parent, View convertView, Object itemData, int position, int viewType)
+    {
+        internalViewHolder.updateViewDimension(parent, convertView, itemData, position, viewType);
+    }
+
+    @Override
+    public void fillData(ViewGroup parent, View convertView, Object itemData, int position, int viewType)
+    {
+        internalViewHolder.updateViewDimension(parent, convertView, itemData, position, viewType);
+    }
+}
