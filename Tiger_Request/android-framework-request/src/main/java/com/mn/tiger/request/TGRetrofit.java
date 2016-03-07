90928becc0e0f4482d9795b9de1b1546e5d5cebc
peng.liu <peng.liu@stylrplus.com>
2016-03-07 13:45:22 +0800

    新增appendParams方法

--- Tiger_Request/android-framework-request/src/main/java/com/mn/tiger/request/TGRetrofit.java
+++ Tiger_Request/android-framework-request/src/main/java/com/mn/tiger/request/TGRetrofit.java
@@ -4,17 +4,32 @@ import android.app.Activity;
 import android.content.Context;
 
 import com.mn.tiger.app.TGActionBarActivity;
+import com.mn.tiger.log.Logger;
+
+import java.util.Iterator;
+import java.util.Map;
 
 import okhttp3.FormBody;
+import okhttp3.HttpUrl;
+import okhttp3.MultipartBody;
+import okhttp3.Request;
 import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.Response;
 
 /**
  * Created by Dalang on 2016/3/6.
+ * Retrofit工具类
  */
 public class TGRetrofit
 {
+    private static final Logger LOG = Logger.getLogger(TGRetrofit.class);
+
+    /**
+     * 将RequestBody输出为字符串
+     * @param requestBody
+     * @return
+     */
     protected static String stringify(RequestBody requestBody)
     {
         StringBuilder builder = new StringBuilder();
@@ -33,8 +48,109 @@ public class TGRetrofit
         return builder.toString();
     }
 
+    /**
+     * 想Request中添加参数
+     * @param originalRequest
+     * @param params
+     * @return
+     */
+    protected static Request appendParams(Request originalRequest, Map<String, String> params)
+    {
+        if(originalRequest.method().equalsIgnoreCase("GET"))
+        {
+            //添加基础参数
+            HttpUrl.Builder urlBuilder = originalRequest.url().newBuilder();
+            Iterator<Map.Entry<String,String>> iterator = params.entrySet().iterator();
+            while (iterator.hasNext())
+            {
+                Map.Entry<String,String> entry = iterator.next();
+                urlBuilder.addQueryParameter(entry.getKey(), entry.getValue());
+            }
+            HttpUrl url = urlBuilder.build();
+            LOG.i("[Method:appendParams] method = " + originalRequest.method() + " ; url = " + url);
+            return originalRequest.newBuilder().url(url).build();
+        }
+        else if(originalRequest.method().equals("POST"))
+        {
+            RequestBody requestBody = originalRequest.body();
+            if(requestBody instanceof FormBody)
+            {
+                //添加基础数据
+                FormBody.Builder formBodyBuilder = new FormBody.Builder();
+                FormBody formBody = (FormBody)requestBody;
+                int size = formBody.size();
+                for (int i = 0;i < size; i++)
+                {
+                    formBodyBuilder.addEncoded(formBody.encodedName(i), formBody.encodedValue(i));
+                }
+
+                Iterator<Map.Entry<String,String>> iterator = params.entrySet().iterator();
+                while (iterator.hasNext())
+                {
+                    Map.Entry<String,String> entry = iterator.next();
+                    formBodyBuilder.add(entry.getKey(), entry.getValue());
+                }
+                FormBody newFormBody = formBodyBuilder.build();
+                LOG.i("[Method:appendParams] method = " + originalRequest.method() + " ; url = " + originalRequest.url());
+                LOG.i("[Method:appendParams] params = " + stringify(newFormBody));
+                return originalRequest.newBuilder().post(newFormBody).build();
+            }
+            else if(requestBody instanceof MultipartBody)
+            {
+                //添加基础数据
+                MultipartBody.Builder multipartBodyBuilder = new MultipartBody.Builder();
+                MultipartBody multipartBody = (MultipartBody)requestBody;
+                multipartBodyBuilder.setType(multipartBody.type());
+                int size = multipartBody.size();
+                for (int i = 0; i < size; i++)
+                {
+                    multipartBodyBuilder.addPart(multipartBody.part(i));
+                }
+                //处理multipart/form-data类型，其他类型后续处理
+                if(multipartBody.type().equals(MultipartBody.FORM))
+                {
+                    Iterator<Map.Entry<String,String>> iterator = params.entrySet().iterator();
+                    while (iterator.hasNext())
+                    {
+                        Map.Entry<String,String> entry = iterator.next();
+                        multipartBodyBuilder.addFormDataPart(entry.getKey(), entry.getValue());
+                    }
+                }
+                else
+                {
+                    FormBody.Builder formBodyBuilder = new FormBody.Builder();
+                    Iterator<Map.Entry<String,String>> iterator = params.entrySet().iterator();
+                    while (iterator.hasNext())
+                    {
+                        Map.Entry<String,String> entry = iterator.next();
+                        formBodyBuilder.add(entry.getKey(), entry.getValue());
+                    }
+                    multipartBodyBuilder.addPart(formBodyBuilder.build());
+                }
+                return originalRequest.newBuilder().post(multipartBodyBuilder.build()).build();
+            }
+        }
+        else if(originalRequest.method().equalsIgnoreCase("PUT"))
+        {
+            LOG.i("[Method:appendParams] method = " + originalRequest.method() + " ; url = " + originalRequest.url());
+        }
+        else if(originalRequest.method().equalsIgnoreCase("DELETE"))
+        {
+            LOG.i("[Method:appendParams] method = " + originalRequest.method() + " ; url = " + originalRequest.url());
+        }
+        return originalRequest;
+    }
+
+    /**
+     * 回调函数
+     * @param <T>
+     * @param <D>
+     */
     public abstract static class Callback<T, D> implements retrofit2.Callback<T>
     {
+        /**
+         * 是否请求成功
+         */
         private boolean success = false;
 
         private Context context;
@@ -44,7 +160,7 @@ public class TGRetrofit
             this.context = context;
         }
 
-        public Context getContext()
+        protected Context getContext()
         {
             return context;
         }
@@ -52,6 +168,7 @@ public class TGRetrofit
         @Override
         public void onResponse(Call<T> call, Response<T> response)
         {
+            //如果界面已销毁,取消call
             if(null != context && context instanceof Activity && ((Activity)context).isFinishing())
             {
                 if(context instanceof TGActionBarActivity)
@@ -61,17 +178,16 @@ public class TGRetrofit
                 return;
             }
 
-            if(call.isCanceled())
+            //从队列中移除call
+            if(null != context && context instanceof TGActionBarActivity)
             {
-                if(null != context && context instanceof TGActionBarActivity)
-                {
-                    ((TGActionBarActivity)context).dequeue(call);
-                }
+                ((TGActionBarActivity)context).dequeue(call);
                 return;
             }
 
             if(!hasError(call, response))
             {
+                //请求成功
                 this.success = true;
                 onRequestSuccess(response, parseOriginalResponse(response));
             }
@@ -79,13 +195,15 @@ public class TGRetrofit
             {
                 if(null != response)
                 {
+                    //请求失败
                     this.success = false;
                     onRequestError(response.code(), response.message(), response);
                 }
                 else
                 {
+                    //请求失败
                     this.success = false;
-                    onRequestError( -1, "", response);
+                    onRequestError(-1, "", response);
                 }
             }
 
@@ -103,29 +221,56 @@ public class TGRetrofit
             onRequestOver(call);
         }
 
-        public boolean hasError(Call<T> call, Response<T> response)
+        /**
+         * 判断是否存在请求异常
+         * @param call
+         * @param response
+         * @return
+         */
+        protected boolean hasError(Call<T> call, Response<T> response)
         {
             return false;
         }
 
+        /**
+         * 请求成功回调方法
+         * @param response
+         * @param data
+         */
         public abstract void onRequestSuccess(Response<T> response, D data);
 
-        public abstract D parseOriginalResponse(Response<T> response);
+        /**
+         * 解析原始结果
+         * @param response
+         * @return
+         */
+        protected abstract D parseOriginalResponse(Response<T> response);
 
+        /**
+         * 请求错误回调方法
+         * @param code
+         * @param message
+         * @param response
+         */
         public void onRequestError( int code ,String message, Response<T> response)
         {
 
         }
 
-        public void onRequestOver(Call<T> call)
+        /**
+         * 请求结束回调方法
+         * @param call
+         */
+        protected void onRequestOver(Call<T> call)
         {
-            if(null != context && context instanceof TGActionBarActivity)
-            {
-                ((TGActionBarActivity)context).dequeue(call);
-            }
+
         }
 
-        public boolean isSuccess()
+        /**
+         * 是否请求成功
+         * @return
+         */
+        protected boolean isSuccess()
         {
             return success;
         }
