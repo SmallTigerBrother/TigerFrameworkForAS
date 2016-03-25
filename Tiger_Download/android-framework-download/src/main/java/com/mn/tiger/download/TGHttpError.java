fd3c8a4d33a4259017044065639c8b2b9cf43ba2
peng.liu <peng.liu@stylrplus.com>
2016-03-25 15:01:07 +0800

    修复字符串名称错误

--- Tiger_Download/android-framework-download/src/main/java/com/mn/tiger/download/TGHttpError.java
+++ Tiger_Download/android-framework-download/src/main/java/com/mn/tiger/download/TGHttpError.java
@@ -16,29 +16,24 @@ public class TGHttpError
 
     public static final int ERROR_URL = 10004;
 
-    public static final int MOCK_TEST_ERROR = 20000;
-
     public static String getDefaultErrorMsg(Context context, int errorCode)
     {
         switch (errorCode)
         {
             case NO_NETWORK:
-                return context.getString(CR.getStringId(context, "http_error_no_network"));
+                return context.getString(CR.getStringId(context, "tiger_http_error_no_network"));
 
             case SOCKET_TIMEOUT:
-                return context.getString(CR.getStringId(context, "http_error_socket_timeout"));
+                return context.getString(CR.getStringId(context, "tiger_http_error_socket_timeout"));
 
             case IOEXCEPTION:
-                return context.getString(CR.getStringId(context, "http_error_ioexception"));
+                return context.getString(CR.getStringId(context, "tiger_http_error_ioexception"));
 
             case UNKNOWN_EXCEPTION:
-                return context.getString(CR.getStringId(context, "http_error_unknown_exception"));
+                return context.getString(CR.getStringId(context, "tiger_http_error_unknown_exception"));
 
             case ERROR_URL:
                 return "";
-
-            case MOCK_TEST_ERROR:
-                return context.getString(CR.getStringId(context, "string.mock_test_error"));
 
             default:
                 break;
