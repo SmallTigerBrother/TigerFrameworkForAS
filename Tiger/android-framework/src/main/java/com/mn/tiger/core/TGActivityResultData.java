6c8a578bbacc3255550c9751a3c93cc5094793ed
peng.liu <peng.liu@stylrplus.com>
2016-07-19 18:33:22 +0800

    移除冗余的Integer类型数据，修改Builder为泛型参数，修改build方法使用反射生成对象

--- Tiger/android-framework/src/main/java/com/mn/tiger/core/TGActivityResultData.java
+++ Tiger/android-framework/src/main/java/com/mn/tiger/core/TGActivityResultData.java
@@ -15,35 +15,23 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class TGActivityResultData implements Serializable
 {
-    private DoubleKeyValueMap<Integer,Boolean, int[]> intToggleArrayMap;
+    protected DoubleKeyValueMap<Integer, Boolean, long[]> longToggleArrayMap;
 
-    private DoubleKeyValueMap<Integer, Boolean, long[]> longToggleArrayMap;
+    protected DoubleKeyValueMap<Integer, Long, Long> longValueMap;
 
-    private DoubleKeyValueMap<Integer, Integer, Integer> intValueMap;
-
-    public int[] getIntArray(int type, boolean selected)
-    {
-        return intToggleArrayMap.get(type, selected);
-    }
-
-    public int[] getIntArray(int type)
-    {
-        return intToggleArrayMap.get(type, true);
-    }
-
-    public long[] getLongArray(int type, boolean selected)
+    protected long[] getLongArray(int type, boolean selected)
     {
         return longToggleArrayMap.get(type, selected);
     }
 
-    public long[] getLongArray(int type)
+    protected long[] getLongArray(int type)
     {
         return longToggleArrayMap.get(type, true);
     }
 
-    public Map<Integer, Integer> getIntValueMap(int type)
+    protected Map<Long, Long> getLongValueMap(int type)
     {
-        return intValueMap.get(type);
+        return longValueMap.get(type);
     }
 
     /**
@@ -52,31 +40,29 @@ public class TGActivityResultData implements Serializable
      */
     public boolean isEmpty()
     {
-        if(null != intToggleArrayMap && intToggleArrayMap.size() > 0)
-        {
-            return false;
-        }
-
         if(null != longToggleArrayMap && longToggleArrayMap.size() > 0)
         {
             return false;
         }
+
+        if(null != longValueMap && longValueMap.size() > 0)
+        {
+            return false;
+        }
+
         return true;
     }
 
-    public static class Builder
+    protected static class Builder<T extends TGActivityResultData>
     {
         private LongToggleMap longToggleMap;
 
-        private IntegerToggleMap intToggleMap;
-
-        private IntegerMap integerValueMap;
+        private LongMap longValueMap;
 
         public Builder()
         {
-            intToggleMap = new IntegerToggleMap();
             longToggleMap = new LongToggleMap();
-            integerValueMap = new IntegerMap();
+            longValueMap = new LongMap();
         }
 
         public Builder append(int type, long data)
@@ -91,38 +77,9 @@ public class TGActivityResultData implements Serializable
             return this;
         }
 
-        public Builder append(int type, int data)
-        {
-            appendToggleData(type, data, false);
-            return this;
-        }
-
-        public Builder append(int type, int[] data)
-        {
-            appendToggleArray(type, data, false);
-            return this;
-        }
-
         public Builder appendToggleData(int type, long data, boolean selected)
         {
             ConcurrentHashMap<Long,Toggle> toggleMap = longToggleMap.get(type);
-            Toggle toggle = toggleMap.get(data);
-            if(null == toggle)
-            {
-                toggle = new Toggle(selected);
-                toggleMap.put(data, toggle);
-            }
-            else
-            {
-                toggle.toggle();
-            }
-
-            return this;
-        }
-
-        public Builder appendToggleData(int type, int data, boolean selected)
-        {
-            ConcurrentHashMap<Integer,Toggle> toggleMap = intToggleMap.get(type);
             Toggle toggle = toggleMap.get(data);
             if(null == toggle)
             {
@@ -162,32 +119,10 @@ public class TGActivityResultData implements Serializable
             return this;
         }
 
-        public Builder appendToggleArray(int type, int[] array, boolean selected)
-        {
-            if(null != array)
-            {
-                ConcurrentHashMap<Integer,Toggle> toggleMap = intToggleMap.get(type);
-                Toggle toggle;
-                for (int data : array)
-                {
-                    toggle = toggleMap.get(data);
-                    if(null == toggle)
-                    {
-                        toggle = new Toggle(selected);
-                        toggleMap.put(data, toggle);
-                    }
-                    else
-                    {
-                        toggle.toggle();
-                    }
-                }
-            }
-            return this;
-        }
 
-        public Builder append(int type, int key, int value)
+        public Builder append(int type, long key, long value)
         {
-            integerValueMap.put(type, key, value);
+            longValueMap.put(type, key, value);
             return this;
         }
 
@@ -213,107 +148,39 @@ public class TGActivityResultData implements Serializable
                 }
             }
 
-            if(null != data.intToggleArrayMap && data.intToggleArrayMap.size() > 0)
+            if(null != data.longValueMap && data.longValueMap.size() > 0)
             {
-                Set<Integer> typeKeys = data.intToggleArrayMap.getFirstKeys();
+                Set<Integer> typeKeys = data.longValueMap.getFirstKeys();
                 Iterator<Integer> iterator = typeKeys.iterator();
                 while (iterator.hasNext())
                 {
                     int type = iterator.next();
-                    int[] intToggleArray = data.intToggleArrayMap.get(type, false);
-                    this.appendToggleArray(type, intToggleArray, false);
-
-                    int[] intReverseToggleArray = data.intToggleArrayMap.get(type, true);
-                    this.appendToggleArray(type, intReverseToggleArray, true);
-                }
-            }
-
-            if(null != data.intValueMap && data.intValueMap.size() > 0)
-            {
-                Set<Integer> typeKeys = data.intValueMap.getFirstKeys();
-                Iterator<Integer> iterator = typeKeys.iterator();
-                while (iterator.hasNext())
-                {
-                    int type = iterator.next();
-                    this.integerValueMap.get(type).putAll(data.intValueMap.get(type));
+                    this.longValueMap.get(type).putAll(data.longValueMap.get(type));
                 }
             }
 
             return this;
         }
 
-        public TGActivityResultData build()
+        public T build()
         {
-            TGActivityResultData resultData = new TGActivityResultData();
-            resultData.intToggleArrayMap = buildIntArrayMap(intToggleMap);
-            resultData.longToggleArrayMap = buildLongArrayMap(longToggleMap);
-            resultData.intValueMap = integerValueMap;
-
-            return resultData;
-        }
-
-        private DoubleKeyValueMap<Integer,Boolean, int[]> buildIntArrayMap(IntegerToggleMap integerToggleMap)
-        {
-            DoubleKeyValueMap<Integer, Boolean, int[]> intArrayMap = new DoubleKeyValueMap<Integer,Boolean, int[]>();
-            if(integerToggleMap.size() > 0)
+            try
             {
-                Iterator<Map.Entry<Integer, ConcurrentHashMap<Integer, Toggle>>> iterator = integerToggleMap.entrySet().iterator();
-                Map.Entry<Integer, ConcurrentHashMap<Integer, Toggle>> entry;
-                ConcurrentHashMap<Integer, Toggle> toggleMap;
-                Iterator<Map.Entry<Integer,Toggle>> toggleIterator;
-                while (iterator.hasNext())
-                {
-                    entry = iterator.next();
-                    toggleMap = entry.getValue();
-                    toggleIterator = toggleMap.entrySet().iterator();
-
-                    ArrayList<Integer>[] arrayLists = buildIntList(toggleIterator);
-                    if(null != arrayLists[0] && arrayLists[0].size() > 0)
-                    {
-                        intArrayMap.put(entry.getKey(),true, Commons.convertIntegerListToIntArray(arrayLists[0]));
-                    }
-
-                    if(null != arrayLists[1] && arrayLists[1].size() > 0)
-                    {
-                        intArrayMap.put(entry.getKey(),false, Commons.convertIntegerListToIntArray(arrayLists[1]));
-                    }
-                }
+                T resultData = (T)Class.forName(Commons.getClassNameOfGenericType(this.getClass(),0)).newInstance();
+                resultData.longToggleArrayMap = buildLongArrayMap(longToggleMap);
+                resultData.longValueMap = longValueMap;
+                return resultData;
             }
-
-            return intArrayMap;
-        }
-
-        private ArrayList<Integer>[] buildIntList(Iterator<Map.Entry<Integer,Toggle>> toggleIterator)
-        {
-            ArrayList<Integer> integerList = new ArrayList<Integer>();
-            ArrayList<Integer> integerReverseList = new ArrayList<Integer>();
-            Map.Entry<Integer, Toggle> toggleEntry;
-            Toggle toggle;
-            while (toggleIterator.hasNext())
+            catch (Exception e)
             {
-                toggleEntry = toggleIterator.next();
-                toggle = toggleEntry.getValue();
-                if(toggle.isChanged())
-                {
-                    if(toggle.getState())
-                    {
-                        integerList.add(toggleEntry.getKey());
-                    }
-                    else
-                    {
-                        integerReverseList.add(toggleEntry.getKey());
-                    }
-                }
+                e.printStackTrace();
             }
-            ArrayList<Integer>[] arrayLists = new ArrayList[2];
-            arrayLists[0] = integerList;
-            arrayLists[1] = integerReverseList;
-            return arrayLists;
+            return null;
         }
 
         private DoubleKeyValueMap<Integer,Boolean, long[]> buildLongArrayMap(LongToggleMap longToggleMap)
         {
-            DoubleKeyValueMap<Integer, Boolean, long[]> longArrayMap = new DoubleKeyValueMap<Integer,Boolean, long[]>();
+            DoubleKeyValueMap<Integer, Boolean, long[]> longArrayMap = new DoubleKeyValueMap<>();
             if(longToggleMap.size() > 0)
             {
                 Iterator<Map.Entry<Integer, ConcurrentHashMap<Long, Toggle>>> iterator = longToggleMap.entrySet().iterator();
@@ -386,37 +253,22 @@ public class TGActivityResultData implements Serializable
             ConcurrentHashMap<Long, Toggle> value = super.get(key);
             if(null == value)
             {
-                value = new ConcurrentHashMap<Long, Toggle>();
+                value = new ConcurrentHashMap<>();
                 super.put(key, value);
             }
             return value;
         }
     }
 
-    private static class IntegerToggleMap extends DoubleKeyValueMap<Integer, Integer, Toggle>
+    private static class LongMap extends DoubleKeyValueMap<Integer, Long, Long>
     {
         @Override
-        public ConcurrentHashMap<Integer, Toggle> get(Integer key)
+        public ConcurrentHashMap<Long, Long> get(Integer key1)
         {
-            ConcurrentHashMap<Integer, Toggle> value = super.get(key);
+            ConcurrentHashMap<Long, Long> value = super.get(key1);
             if(null == value)
             {
-                value = new ConcurrentHashMap<Integer, Toggle>();
-                this.put(key, value);
-            }
-            return value;
-        }
-    }
-
-    private static class IntegerMap extends DoubleKeyValueMap<Integer, Integer, Integer>
-    {
-        @Override
-        public ConcurrentHashMap<Integer, Integer> get(Integer key1)
-        {
-            ConcurrentHashMap<Integer, Integer> value = super.get(key1);
-            if(null == value)
-            {
-                value = new ConcurrentHashMap<Integer, Integer>();
+                value = new ConcurrentHashMap<>();
                 this.put(key1, value);
             }
             return value;
