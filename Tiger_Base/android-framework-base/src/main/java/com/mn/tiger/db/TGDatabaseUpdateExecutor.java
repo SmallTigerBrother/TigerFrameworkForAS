499905aec24ee4aa2f872d4221cf12d71e8446c5
peng.liu <peng.liu@stylrplus.com>
2016-08-31 15:54:42 +0800

    修改执行方法在事务中执行，新增部分快捷方法

--- Tiger_Base/android-framework-base/src/main/java/com/mn/tiger/db/TGDatabaseUpdateExecutor.java
+++ Tiger_Base/android-framework-base/src/main/java/com/mn/tiger/db/TGDatabaseUpdateExecutor.java
@@ -21,7 +21,11 @@ public abstract class TGDatabaseUpdateExecutor
         this.toVersion = toVersion;
     }
 
-    void execute(SQLiteDatabase db)
+    /**
+     * 执行更新操作
+     * @param db
+     */
+    final void execute(SQLiteDatabase db)
     {
         LOG.d("[Method:execute] fromVersion == " + fromVersion + " toVersion == " + toVersion + " start!");
         //开始事务
@@ -53,13 +57,80 @@ public abstract class TGDatabaseUpdateExecutor
         return toVersion;
     }
 
+    /**
+     * 在事务中更新数据库
+     * @param db
+     * @param fromVersion
+     * @param toVersion
+     */
     protected abstract void updateDatabaseInTransaction(SQLiteDatabase db, int fromVersion, int toVersion);
 
-    protected void renameColumn(SQLiteDatabase db,String tableName, String originalColumn, String newColumn)
+    /**
+     * 新增字段
+     * @param db
+     * @param tableName
+     * @param columnName
+     * @param dataType
+     * @param defaultValue
+     * @param notNull
+     */
+    protected void addColumn(SQLiteDatabase db, String tableName, String columnName, String dataType, Object defaultValue, boolean notNull)
     {
-        db.execSQL("ALERT TABLE " + tableName + " CHANGE COLUMN " + originalColumn + " " + newColumn);
+        StringBuilder sqlBuilder = new StringBuilder();
+        sqlBuilder.append("ALERT TABLE ");
+        sqlBuilder.append(tableName);
+        sqlBuilder.append(" ADD COLUMN ");
+        sqlBuilder.append(columnName);
+        sqlBuilder.append(" " + dataType + " ");
+        if (notNull)
+        {
+            sqlBuilder.append(" NOT NULL ");
+        }
+
+        if(null != defaultValue)
+        {
+            sqlBuilder.append("DEFAULT " + defaultValue);
+        }
     }
 
+    /**
+     * 删除一个字段
+     * @param tableName
+     * @param columnName
+     */
+    protected void dropColumn(SQLiteDatabase db, String tableName, String columnName)
+    {
+        db.execSQL("ALERT TABLE " + tableName + " DROP COLUMN " + columnName);
+    }
+
+    /**
+     * 重命名一个字段名
+     * @param db
+     * @param tableName
+     * @param originalColumn
+     * @param newColumn
+     */
+    protected void renameColumn(SQLiteDatabase db, String tableName, String originalColumn, String newColumn)
+    {
+        db.execSQL("ALERT TABLE " + tableName + " RENAME COLUMN " + originalColumn + " TO " + newColumn);
+    }
+
+    /**
+     * 重命名一张表
+     * @param db
+     * @param originalTable
+     * @param toTable
+     */
+    protected void renameTable(SQLiteDatabase db, String originalTable, String toTable)
+    {
+        db.execSQL("ALERT TABLE " + originalTable + " RENAME TO " + toTable);
+    }
+
+    /**
+     * 删除
+     * @param db
+     * @param tableName
+     */
     protected void dropTable(SQLiteDatabase db, String tableName)
     {
         db.execSQL("DROP TABLE IF EXISTS" + tableName);
